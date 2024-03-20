a71a61c3d8799b82ec3794e5cdccaaa23ec56c9e
Eugene <eugene@blust.ai>
2024-03-20 14:08:16 -0700

    Corrector

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# Blust AI Template for Frontend Based on React + Vite + MUI
+# Blust AI | Corrector
 
-Clone the project and run `pnpm install` (`npm` works too).
-
-Please ensure you are running this App on `localhost:5173`; otherwise, you will encounter a CORS error. Alternatively, you can add your frontend URL as an external website to your AI tool ([Details](https://docs.blust.ai/docs/creating-ai-tools/integrations/website/)).
+This custom AI tool is based on [Blust AI custom tool template](https://github.com/blustAI/blust-ai-custom-tool-template)
--- src/pages/CustomTool/CustomTool.jsx
+++ src/pages/CustomTool/CustomTool.jsx
@@ -1,79 +1,191 @@
-import { Alert, Button, Card, CircularProgress, Container, Grid, Paper, TextField } from "@mui/material";
-import { useWss } from "blustai-react-core";
-import { useEffect,  useState } from "react";
+import { Alert, AlertTitle, Typography, Box, Button, IconButton, CircularProgress, Container, FormControl, Grid, InputLabel, LinearProgress, MenuItem, InputAdornment, Select, TextField } from "@mui/material";
+import { useWss, restAPI } from "blustai-react-core";
+import { useEffect, useState } from "react";
+import { CloseIcon,CopyToClipboardButton} from "blustai-react-ui";
 
 const service_id = import.meta.env.VITE_TOOL_ID //SET YOUR TOOL ID HERE 
 
+const CorrectionReview = (props) => {
+    const { summary, text, errors } = props;
+    return <Alert severity={errors.length ? "error" : "success"}>
+        <AlertTitle>{summary}</AlertTitle>
+        {errors?.map((error,key) => (
+            <Box sx={{pt:2}} key={key} >
+                <span className={'correction_box correcttion_'+error.type} title={error.type}> </span>
+                <Typography variant="dashed">{error.context}</Typography>{'  -  '}
+                <Typography variant="caption">{error.description}</Typography>
+            </Box>
+        ))}
+    </Alert>
+}
+
+
 const CustomTool = () => {
     const { client } = useWss();
-    const [chatId, setChatId] = useState();
-    const [response, setResponse] = useState();
-    const [images, setImages] = useState();
+    const [mode, setMode] = useState(localStorage.getItem("corrector_mode"));
+    const [modes, setModes] = useState([]);
+    const [initializing, setInitializing] = useState(true);
     const [error, setError] = useState();
     const [submitting, setSubmitting] = useState();
+    const [originalText,setOriginalText]=useState('');
+    const [response, setResponse] = useState();
+    const [dirty,setDirty]=useState(false);
+
+
 
     useEffect(() => {
-        client.init({
-            onReady: () => {
-                console.log("Blust AI Client ready");
-                //if you know chatId - you can load history: client.getHistory({service: service_id, chat:chatId}) 
-                //or you can get history for last chat: client.getHistory({service: service_id})
-            },
-            onError: (error) => setError(error?.error || error?.message || "Blust AI Client init error")
+        setInitializing(true)
+        restAPI.list("public-text-modes", { sort: '{"sort":1}' }).then(_modes => {
+            setModes(_modes);
+            if (!mode && _modes?.length) setMode(_modes[0].name)
+            //_data[0]?.name
+            return client.init({
+                onReady: () => {
+                    console.log("Blust AI Client ready");
+                },
+                onError: (error) => setError(error?.error || error?.message || "Blust AI Client init error")
+            });
+        }).then(() => {
+            setInitializing(false);
+        }).catch(err => {
+            toast.error(err.error || err.message || "Error loading data");
+            setInitializing(false);
+            setError(err?.message || err?.error || "There is an error occuried. Please try again later")
         });
     }, [])
 
-    const onSubmit = async (e) => {
-        e.preventDefault();
-        console.log("here",e.target.elements.prompt.value)
-        if (!e.target.elements.prompt.value) { setError("Please, input prompt"); return };
+
+    const handleModeChange=(e)=>{
+        setMode(e.target.value);
+        setDirty(true);
+        localStorage.setItem("corrector_mode", e.target.value);
+    }
+    
+    const onSubmit = async () => {
+        console.log("using service",service_id)
+        if (!originalText) { toast.error("Please, input text"); return };
         setSubmitting(true);
         try {
             setError(null);
             setResponse(null);
+            setDirty(false);
             let _response = await client.sendMessage({
                 service: service_id,
-                chat: chatId, //if null - new thread will be created
-                message: e.target.elements.prompt.value,
-                onStream: (text) => setResponse(text),
-                //voice: "VOICE_FILE_URL", //you can use audio files as a prompt (if voice recognition enabled)
-                //attachments: [{type:"image",url:"IMAGE_URL"}] //you can attach images (if image recognition enabled)
+                featured: true,
+                message: originalText,
+                params: {mode}
             });
-            setResponse(_response?.body);
-            if (_response?.images?.length) setImages(_response.images)
-            //_response?.files will contain files (if files are generated and if your tool support file parsing)
-            //_response?.voice will contain voice response (if audio output is enabled)
-            if (!chatId) setChatId(response?.chat); //saving chatId if not set
+            setResponse(_response);
         } catch (error) {
-            setError(error?.error || error?.message || "Sending message error")
+            toast.error(error?.error || error?.message || "Sending message error")
         }
         setSubmitting(false);
     }
 
-    return <Container maxWidth="md" >
-        <Card sx={{ p: 4, m: 4 }}>
-            {error &&
-                <Alert severity="error" sx={{ mb: 2 }}>{error}</Alert>
-            }
-            {!service_id ?
-                <Alert severity="error" sx={{ mb: 2 }}>Please, set service_id</Alert>
-                :
-                <form action="#" onSubmit={onSubmit}>
-                    <TextField name="prompt"  label="Prompt" rows={5} multiline fullWidth />
-                    <Button 
-                        type="submit" 
-                        sx={{ mt: 2 }} 
-                        disabled={submitting}
-                    >Send {submitting && <CircularProgress size={"1em"}/>}
-                    </Button>
-                    {response &&
-                        <Paper sx={{p:2,mt:2,mb:2}}>{response}</Paper>
-                    }
-                    {images?.map((img, key) => <img key={key} src={img.url} width="150" />)}
-                </form>
-            }
-        </Card>
-    </Container>
+    return <>
+        {initializing ?
+            <LinearProgress color="info" />
+            :
+            <Container maxWidth="md" sx={{ pt: 5 }}>
+                {error ?
+                    <Alert severity="error" sx={{ mb: 2 }}>{error}</Alert>
+                    :
+                    <Grid container spacing={2} >
+                        <Grid item xs={12} sx={{ textAlign: 'right', mt: 0.5 }}>
+                            <Box sx={{ display: 'flex', justifyContent: 'end' }}>
+                                <FormControl size="small" variant="standard">
+                                    <InputLabel id="mode-label">Mode</InputLabel>
+                                    <Select
+                                        value={mode}
+                                        onChange={handleModeChange}
+                                        labelId="mode-label"
+                                        label="Mode"
+                                        disabled={submitting}
+                                    >
+                                        {modes?.map((mode, key) => (
+                                            <MenuItem
+                                                key={key}
+                                                value={mode.name}
+                                            >
+                                                {mode.name}
+                                            </MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+                            </Box>
+                            <TextField
+                                id="original"
+                                variant="filled"
+                                fullWidth
+                                multiline
+                                maxRows={40}
+                                minRows={6}
+                                inputProps={{ maxLength: 5000 }}
+                                InputProps={{
+                                    disableUnderline: true,
+                                    style: { borderSize: '1px', borderColor: '#ccc', padding: '1em', paddingRight: '2.5em' },
+                                    ...originalText?.length ? {
+                                        endAdornment: <InputAdornment position="end" sx={{ position: 'absolute', right: '0.2em', top: '1.5em' }}>
+                                            <IconButton onClick={() => {setOriginalText('');setResponse()}}><CloseIcon /></IconButton>
+                                        </InputAdornment>,
+                                        startAdornment: <InputAdornment position="start" sx={{ position: 'absolute', right: '0.2em', bottom: '1.5em' }}>
+                                            <Typography variant="caption" >{originalText.length} / 5000</Typography>
+                                        </InputAdornment>
+                                    } : {}
+                                }}
+                                sx={{ borderColor: '#ccc', borderSize: '1px', position: 'relative' }}
+                                value={originalText}
+                                onChange={(e)=>{setOriginalText(e.target.value);setDirty(true)}}
+                                disabled={submitting}
+                                placeholder={'Type the text...'}
+                                autoFocus
+                            />
+                            {originalText?.trim()?.length > 0 &&
+                                <Button variant="contained" onClick={onSubmit} sx={{ mt: 2 }} disabled={originalText.trim() === "" || submitting|| !dirty}>
+                                    Correct mistakes
+                                    {submitting &&
+                                        <CircularProgress size={20} sx={{ ml: 1 }} />
+                                    }
+                                </Button>
+                            }
+                        </Grid>
+                        {response?.body &&
+                            <Grid item xs={12} >
+                                {response.summary &&
+                                    <CorrectionReview
+                                        summary={response.summary}
+                                        text={originalText}
+                                        errors={response.errors}
+                                    />
+                                }
+                                <TextField
+                                    id="translation"
+                                    variant="filled"
+                                    fullWidth
+                                    multiline
+                                    maxRows={40}
+                                    minRows={6}
+                                    value={response.body}
+                                    {...submitting ? {
+                                        className: "loading-bg"
+                                    } : {}}
+                                    InputProps={{
+                                        readOnly: true,
+                                        disableUnderline: true,
+                                        style: { borderSize: '1px', borderColor: '#ccc', padding: '1em', paddingRight: '2.5em' },
+                                        endAdornment: <InputAdornment position="end" sx={{ position: 'absolute', right: '0.2em', top: '1.5em' }}>
+                                            <CopyToClipboardButton text={response.body} size="small"  />
+                                        </InputAdornment>
+                                    }}
+                                />
+                            </Grid>
+                        }
+                    </Grid>
+                }
+            </Container>
+        }
+    </>
+
 }
 
 export default CustomTool;
